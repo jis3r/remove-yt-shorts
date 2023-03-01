bf1ee4270fd82a391cb989360a509373d7c45048
jis3r <jis3r@protonmail.com>
2023-03-01 20:43:08 +0100

    fix: removed 'console.log'

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 const PARENTS = 12;
+
 function removeShortsSpans() {
     document.querySelectorAll('span#text').forEach((span) => {
       if (span.textContent.trim() === 'SHORTS') {
-        console.log('SHORTS');
         let el = span.parentElement;
         for (let i = 0; i < PARENTS && el; i++) {
           el = el.parentElement;
